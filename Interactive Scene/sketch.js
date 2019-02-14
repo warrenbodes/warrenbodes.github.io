fcf41f0e763a8d95aaf02a8d1355ea3c184295b1
warrenbodes <47368856+warrenbodes@users.noreply.github.com>
2019-02-14 15:05:34 -0600

    First assignment created

---
+++ Interactive Scene/sketch.js
@@ -0,0 +1,16 @@
+// Interactive Scene
+// Pouya Pourhaj
+// March 1, 2019
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+    background(220);
+    ellipse(mouseX, mouseY, 50, [50]);
+}
